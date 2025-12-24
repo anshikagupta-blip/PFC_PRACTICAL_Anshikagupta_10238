5aed7e61494c30beede5d629ce96d8218eeb3487
unknown <anshika.gupta@rungta.org>
2025-12-24 13:46:47 +0530

    pract6

---
+++ praclical6/hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>1 4
+int main(){
+    //Anshika Gupta
+    //ERP 10238
+    int a,b;
+    printf("enter a and b:");
+    scanf("%d %d",&a,&b);
+
+    for(int i=a;i<=b;i++){
+        printf("%d",i);
+    }
+    return 0;
+}
+//output
+//enter a and b:2 5
+// 2345
