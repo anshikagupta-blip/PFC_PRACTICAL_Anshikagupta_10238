4f4610d46f9ddeefdef5da79cb89054e06bc5f67
unknown <anshika.gupta@rungta.org>
2025-12-25 17:16:50 +0530

    practical 6,7,8

---
+++ practical8/sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//Anshika Gupta 
+//ERP 10238
+
+int sumOfDigits(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        sum = sum + (n % 10);
+        n = n / 10;
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    printf("sum of digit = %d", sumOfDigits(n));
+    return 0;
+    
+}
